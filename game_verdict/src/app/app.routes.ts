4d339c31620cb793c236763bfcd2bb1ff4fc1407
SamiChafik <sami.chafik00@gmail.com>
2025-09-01 14:33:46 +0100

    new route added

--- game_verdict/src/app/app.routes.ts
+++ game_verdict/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { AuthGuard } from './guard/auth.guard';
+import { AdminGuard } from './guard/admin.guard';
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent},
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'dashboard', component: DashboardComponent}
+  { path: 'dashboard', component: DashboardComponent, canActivate:[AuthGuard, AdminGuard]}
 ];
