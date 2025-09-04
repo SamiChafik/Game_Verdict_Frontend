b0114fb14e679c4021b9ae005ce50b6c09e264e4
SamiChafik <sami.chafik00@gmail.com>
2025-09-04 19:31:17 +0100

    new route/guard

--- game_verdict/src/app/app.routes.ts
+++ game_verdict/src/app/app.routes.ts
@@ -5,10 +5,13 @@ import { RegisterComponent } from './components/register/register.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { AuthGuard } from './guard/auth.guard';
 import { AdminGuard } from './guard/admin.guard';
+import { BannedComponent } from './components/banned/banned.component';
+import { BanGuard } from './guard/ban.guard';
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent},
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'dashboard', component: DashboardComponent, canActivate:[AuthGuard, AdminGuard]}
+  { path: 'dashboard', component: DashboardComponent, canActivate:[AuthGuard, AdminGuard, BanGuard]},
+  { path: 'banned', component: BannedComponent }
 ];
