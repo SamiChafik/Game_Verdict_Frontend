c45cd0bb5415d979a00ab4f0824f40cbb7700ac9
SamiChafik <sami.chafik00@gmail.com>
2025-09-13 20:23:58 +0100

    new paths added

--- game_verdict/src/app/app.routes.ts
+++ game_verdict/src/app/app.routes.ts
@@ -7,11 +7,15 @@ import { AuthGuard } from './guard/auth.guard';
 import { AdminGuard } from './guard/admin.guard';
 import { BannedComponent } from './components/banned/banned.component';
 import { BanGuard } from './guard/ban.guard';
+import { GameListComponent } from './components/game-list/game-list.component';
+import { FavoriteGamesComponent } from './components/favorite-games/favorite-games.component';
 
 export const routes: Routes = [
-  { path: 'home', component: HomeComponent},
+  { path: 'home', component: HomeComponent, canActivate: [BanGuard]},
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'dashboard', component: DashboardComponent, canActivate:[AuthGuard, AdminGuard, BanGuard]},
-  { path: 'banned', component: BannedComponent }
+  { path: 'banned', component: BannedComponent },
+  { path: 'games', component: GameListComponent, canActivate: [BanGuard]},
+  { path: 'favorites', component: FavoriteGamesComponent, canActivate: [AuthGuard, BanGuard]},
 ];
